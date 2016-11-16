8f6b9acef900901a1920001fac9f9ff24bcf7a69
Jaakko Virtanen <jaakvirt@dhcp-asv-94.helsinki.fi>
2016-11-16 18:03:51 +0200

    eriytetty sql-lataukset yhdestä dumpista kolmeen, ohjeen mukaisesti

--- add_test_data.sh
+++ add_test_data.sh
@@ -6,7 +6,7 @@ echo "Lisätään testidata..."
 
 ssh $USERNAME@users.cs.helsinki.fi "
 cd htdocs/$PROJECT_FOLDER/sql
-psql < add_test_data.sql
+mysql -uroot -pIOfvaLu < add_test_data.sql
 exit"
 
 echo "Valmis!"
