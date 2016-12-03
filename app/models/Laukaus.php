ba061eaaca0ef46ed93386699337af0970f01665
Jaakko Virtanen <jaakvirt@dhcp-asv-94.helsinki.fi>
2016-12-04 00:04:41 +0200

    vieraslaukaukset oikein

--- app/models/Laukaus.php
+++ app/models/Laukaus.php
@@ -2,14 +2,14 @@
 
 class Laukaus extends BaseModel {
 
-    public $idLaukaisut, $pelaajaTunnus, $top, $lefty, $otteluTunnus, $joukkue, $aika, $tulos, $event;
+    public $idLaukaisut, $pelaajaTunnus, $top, $lefty, $otteluTunnus, $joukkue, $aika, $tulos, $event, $isHome;
 
     public function __construct($attributes) {
         parent::__construct($attributes);
     }
 
     public static function all() {
-        $query = DB::connection()->prepare('SELECT * FROM Laukaisut');
+        $query = DB::connection()->prepare('SELECT *, l.joukkue=o.kotijoukkueid as isHome FROM Laukaisut l JOIN Ottelut o ON o.otteluTunnus = l.otteluTunnus');
         $query->execute();
         $rows = $query->fetchAll();
         $laukaisut = array();
@@ -24,9 +24,17 @@ class Laukaus extends BaseModel {
                 'joukkue' => $row['joukkue'],
                 'aika' => $row['aika'],
                 'tulos' => $row['tulos'],
-                'event' => $row['event']
+                'event' => $row['event'],
+                'isHome'=>$row['isHome']
             ));
         }
         return $laukaisut;
+    }
+    
+    public function isHome() {
+        if ($this->isHome == '1') {
+            return 'home';
+        }
+        return 'away';
     }
 }
