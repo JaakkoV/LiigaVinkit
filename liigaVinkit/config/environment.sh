0abd17156ead7c12486e95e283e47c957001c08d
JaakkoV <jaakvirt@cs.helsinki.fi>
2017-07-27 22:14:54 +0300

    move all back

--- liigaVinkit/config/environment.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# korvaa tämä omalla käyttäjätunnuksellasi
-USERNAME="jaakvirt"
-# korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER="liigaVinkit"
-# sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
