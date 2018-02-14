a24dbee20310b84942be3edeb4c518ab8e2acb2a
JaakkoV <jaakvirt@cs.helsinki.fi>
2018-02-14 23:14:03 +0200

    Configure back from psql --> mysql

--- config/database.php
+++ config/database.php
@@ -1,26 +1,5 @@
 <?php
 
-
-  # This function reads your DATABASE_URL config var and returns a connection
-  # string suitable for pg_connect. Put this in your app.
-  function pg_connection_string_from_database_url() {
-    extract(parse_url($_ENV["DATABASE_URL"]));
-    return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
-  }
-  # Here we establish the connection. Yes, that's all.
-  $pg_conn = pg_connect(pg_connection_string_from_database_url());
-  # Now let's use the connection for something silly just to prove it works:
-  $result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
-  print "<pre>\n";
-  if (!pg_num_rows($result)) {
-    print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
-  } else {
-    print "Tables in your database:\n";
-    while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
-  }
-  print "\n";
-
-
   class DatabaseConfig{
     // Valitse käyttämäsi tietokantapalvelin - PostgreSQL (psql) tai MySQL (mysql)
     private static $use_database = 'mysql';
@@ -32,9 +11,7 @@
         'resource' => 'pgsql:'
       ),
       'mysql' => array(
-        'resource' => 'mysql:unix_socket=/home/jaakvirt/mysql/socket;dbname=liiga',
-        'username' => 'root',
-        'password' => 'IOfvaLu'
+        'resource' => 'mysql://bc010aef102bb6:72fb1428@us-cdbr-iron-east-05.cleardb.net/heroku_4977b88ee16b95e?reconnect=true',
       )
     );
 
